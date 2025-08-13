90f318f4839292e533c49ec2d586fe932d0366b5
Geovanne Washington <geoxp98@gmail.com>
2025-08-13 17:50:11 -0300

    Add initial typescript overview

---
+++ traversy-media/setup-and-compiler/index.js
@@ -0,0 +1,6 @@
+"use strict";
+// TypeScript also infers types, it will throw an error if you try to assign a non-number to this 
+// variable, but it will still compile to JavaScript.
+Object.defineProperty(exports, "__esModule", { value: true });
+let id = 10;
+let user = "Geovanne";
---
+++ traversy-media/setup-and-compiler/index.ts
@@ -0,0 +1,9 @@
+// TypeScript also infers types, it will throw an error if you try to assign a non-number to this 
+// variable, but it will still compile to JavaScript.
+
+let id: number = 10;
+let user: string = "Geovanne";
+
+
+
+
