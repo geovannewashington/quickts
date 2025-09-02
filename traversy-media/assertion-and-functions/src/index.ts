5481018ad79d099b7ee8e76613d644c6f387cf46
Geovanne Washington <geoxp98@gmail.com>
2025-09-02 16:49:16 -0300

    Add type assertion explanation

---
+++ traversy-media/assertion-and-functions/src/index.ts
@@ -0,0 +1,31 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// Type Assertion:
+ 
+// Type assertion in TypeScript is a way of telling the compiler “trust me, I know more about the 
+// type of this value than you do.”
+
+// There are two syntax of type assertion:
+// 1. Angle-bracket system:
+let value: unknown = "box";
+let length: number = (<string>value).length; 
+console.log(`The word '${value}' contains ${length} letters.`); // The word 'geo'contains 3 letters.
+
+// 2. as syntax (preferred, especially in JSX/TSX):
+let value1: any = "dog";
+let length_1: number = (value1 as string).length;
+        
+// The word sun also contains 3 letters.
+const message: string = (length === length_1) 
+    ? `The word '${value1}' also contains ${length} letters.`
+    : `The word '${value1}' contains ${length_1} letters.`;
+
+console.log(message);
+
+// When to use type assertion? 
+// -> when narrowing (refining it to a more specific type) a untion type:
+let someValue: string | number;
+
+someValue = "Hello";
+
+// Compiler doesn't know it's a string here:
+let strLength: number = (someValue as string).length; // alternatively (<string>someValue).length;
